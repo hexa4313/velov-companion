c2f611465cc5de055f00a7b11470d56a9072f32f
admin <admin@justine-2.home>
2015-05-02 12:30:28 +0200

    profil

--- companion/www/js/loginController.js
+++ companion/www/js/loginController.js
@@ -1,11 +1,19 @@
 angular.module('vc.loginController', [])
 
-.controller('loginController', function($scope, LoginService, $ionicPopup, $state) {
+.controller('loginController', function($scope, LoginService, $ionicPopup, $state, $http) {
     $scope.data = {};
- 
+    $scope.user = {};
+    if(LoginService.isLogged()){
+        $scope.user = LoginService.getUser();
+    }
+            
+    $scope.isLogged = function() { return LoginService.isLogged(); }
+    
     $scope.login = function() {
-        LoginService.loginUser($scope.data.username, $scope.data.password).success(function(data) {
-            $state.go('profilConnecte');
+        LoginService.loginUser($scope.data.username, $scope.data.password, $http).success(function(data) {
+            
+            $state.reload();
+            
         }).error(function(data) {
             var alertPopup = $ionicPopup.alert({
                 title: 'Login failed!',
