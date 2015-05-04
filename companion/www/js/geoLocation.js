16cfcf675d9e01f65a3f4f57c803839e4141c219
modou6c <contact.modoucisse@gmail.com>
2015-05-04 11:04:08 +0200

    itineraire and user services

--- companion/www/js/geoLocation.js
+++ companion/www/js/geoLocation.js
@@ -4,20 +4,20 @@
 
 angular.module('vc').factory('geoLocation', function ($localStorage) {
 
-        return {
-            setGeolocation: function (latitude, longitude) {
-                var userPosition = {
-                    latitude: latitude,
-                    longitude: longitude
-                };
-                $localStorage['geoLocation'] = userPosition;
-            },
-            getGeolocation: function () {
-                return glocation = {
-                    lat: $localStorage['geoLocation'].latitude,
-                    lng: $localStorage['geoLocation'].longitude
-                }
+    return {
+        setGeolocation: function (latitude, longitude) {
+            var userPosition = {
+                latitude: latitude,
+                longitude: longitude
+            };
+            $localStorage['geoLocation'] = userPosition;
+        },
+        getGeolocation: function () {
+            return glocation = {
+                lat: $localStorage['geoLocation'].latitude,
+                lng: $localStorage['geoLocation'].longitude
             }
-        };
+        }
+    };
 
 });
--- companion/www/js/stations.js
+++ companion/www/js/stations.js
@@ -16,6 +16,33 @@ angular.module('vc').factory('Stations', function ($localStorage) {
                         return stt;
                     }
                 }
+            },
+            setBookmarks: function (stations) {
+                $localStorage['bookmarks'] = stations;
+            },
+            getBookmarks: function () {
+                return $localStorage['bookmarks'];
+            },
+            isBookmark: function (idBookmark) {
+                if(!$localStorage['bookmarks']) {
+                    return false;
+                }
+                var result = false;
+                for(bm of $localStorage['bookmarks']) {
+                    if(bm.number == idBookmark) {
+                        result= true;
+                        break;
+                    }
+                    return result;
+                };
+            },
+            hasBookmark : function () {
+                if($localStorage['stations']) {
+                    return true;
+                }
+                else {
+                    return false;
+                }
             }
         };
 
